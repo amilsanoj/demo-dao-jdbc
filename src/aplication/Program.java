f071355a46349271ee105259fe9a356669199410
Jonas Lima <jonasgomeslimajunior@gmail.com>
2024-02-27 18:28:52 -0300

    Seller Entity Class

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,10 +1,15 @@
 package aplication;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "books");
+        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.00, obj);
         System.out.println(obj);
+        System.out.println(seller);
     }
 }
