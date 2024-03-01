5c72bd5d3f13a867968bba267a03b460759e0378
Jonas Lima <jonasgomeslimajunior@gmail.com>
2024-02-29 23:58:42 -0300

    Department correct

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -4,7 +4,7 @@ import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Seller implements Serializable {
+public class Department implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
     private Integer id;
