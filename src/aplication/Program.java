2f64fe4d82a46e71ddabf079292fc8a55968468e
Jonas Lima <jonasgomeslimajunior@gmail.com>
2024-04-08 21:15:25 -0300

    FindById

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -9,11 +9,8 @@ import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1, "books");
-        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.00, obj);
-        System.out.println(obj);
-        System.out.println(seller);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
     }
 }
