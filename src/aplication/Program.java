2998c112bdb67e1002ad0a94fe02e6de9d89a1d3
Jonas Lima <jonasgomeslimajunior@gmail.com>
2024-04-09 20:08:14 -0300

    FindByDepartment

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -6,11 +6,17 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+        Department department = new Department(2,null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for(Seller obj : list){
+            System.out.println(obj);
+        }
     }
 }
